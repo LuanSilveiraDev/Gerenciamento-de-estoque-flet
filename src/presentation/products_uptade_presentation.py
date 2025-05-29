b65f8f6b29006ef3a132f4839ee7e895c2068298
Luan silveira <luansilveira022@gmal.com>
2025-05-28 23:05:00 -0300

    Criei alertas de mensagens na hora de atualizar a tabela

--- src/presentation/products_uptade_presentation.py
+++ src/presentation/products_uptade_presentation.py
@@ -1,4 +1,5 @@
 import flet as ft
+from flet_toast import flet_toast
 from models.product_model import Product
 
 def show_update_dialog(page: ft.Page, product: Product, on_update_callback):
@@ -10,16 +11,50 @@ def show_update_dialog(page: ft.Page, product: Product, on_update_callback):
     
     def salvar(e):
         try:
-            product.description = desc_field.value
-            product.mark = mark_field.value
-            product.value = float(value_field.value)
-            product.stock_quantity = int(stock_field.value)
-            on_update_callback(product)
-            dialog.open = False
-            page.update()
+            if not desc_field.value:
+                flet_toast.error(page=page, message="O nome do produto é obrigatório.", duration=5)
+                return
+            elif not mark_field.value:
+                flet_toast.error(page=page, message="A marca do produto é obrigatória.", duration=5)
+                return
+            elif not value_field.value:
+                flet_toast.error(page=page, message="O valor do produto é obrigatório.", duration=5)
+                return
+            elif not stock_field.value:
+                flet_toast.error(page=page, message="A quantidade em estoque é obrigatória.", duration=5)
+                return
+            elif not value_field.value.isdigit():
+                flet_toast.error(page=page, message="O valor deve ser um número.", duration=5)
+                return
+            else:
+                product.description = desc_field.value
+                product.mark = mark_field.value
+                product.value = float(value_field.value)
+                product.stock_quantity = int(stock_field.value)
+                on_update_callback(product)
+                dialog.open = False
+                
+                page.update()
+                return True
         except Exception as ex:
             print("Erro ao atualizar", ex)
-            
+            return False
+        
+    def on_click_salvar(e):
+        if salvar(e):
+            flet_toast.sucess(
+                page=page,
+                message="Produto atualizado com sucesso!",
+                duration=5
+            )
+        elif product.value != ValueError:
+            flet_toast.error(
+                page=page,
+                message="Erro ao atualizar o produto.",
+                duration=5
+            )
+    
+    
     def fechar(e):
         dialog.open = False
         page.update()
@@ -33,7 +68,7 @@ def show_update_dialog(page: ft.Page, product: Product, on_update_callback):
             stock_field,
         ],tight=True),
         actions=[
-            ft.TextButton("Salvar", on_click=salvar),
+            ft.TextButton("Salvar", on_click=on_click_salvar),
             ft.TextButton("Cancelar", on_click=fechar),
         ],
         modal=True
