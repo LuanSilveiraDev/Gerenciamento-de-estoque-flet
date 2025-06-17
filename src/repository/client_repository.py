432b0b6b0fe926d02089313d49b8539fe3f5204b
Luan silveira <luansilveira022@gmal.com>
2025-06-16 22:11:54 -0300

    Repositório do Banco de dados das execuções do cliente

--- src/repository/client_repository.py
+++ src/repository/client_repository.py
@@ -13,3 +13,24 @@ class ClientRepository:
             """, (client.name, client.email)
         )
         self.db.conn.commit()
+        
+    def get_all(self):
+        self.db.cursor.execute(
+            """
+            SELECT id, name, email FROM cliente
+            """
+        )
+        result = self.db.cursor.fetchall()
+        return [
+            Client(id=r[0], name=r[1], email=r[2])
+            for r in result
+        ]
+    
+    def delete_client(self, client_id):
+        self.db.cursor.execute(
+            """
+            DELETE FROM cliente WHERE id = %s
+            """,(client_id,)
+        )
+        self.db.conn.commit()
+        
