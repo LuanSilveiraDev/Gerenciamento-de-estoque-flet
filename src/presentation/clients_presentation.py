7fbb77c605cf478f3f406c742cdc1a95fb4098f6
Luan silveira <luansilveira022@gmal.com>
2025-06-16 22:09:41 -0300

    Lista de clientes e busca de clientes

--- src/presentation/clients_presentation.py
+++ src/presentation/clients_presentation.py
@@ -1,9 +1,11 @@
 import flet as ft
 from services.client_service import ClientService
-
+from models.client_model import Client
+from repository.client_repository import ClientRepository
 
 class ClientUI:
     def __init__(self, refresh_client):
+        self.update = ClientRepository()
         self.service = ClientService()
         self.refresh_client = refresh_client
         
@@ -20,11 +22,7 @@ class ClientUI:
             email_field.value = ""
             self.refresh_client()
             
-
-            
-    
-    
-        
+   
         form_container = ft.Container(
             content=ft.Column(
                 controls=[
@@ -43,4 +41,73 @@ class ClientUI:
         return [form_container,
                 buttom]
         
+    def build_table_client(self):
         
+        search_input = ft.TextField(
+            label="Buscar cliente",
+            hint_text="Digite o nome do Cliente",
+            width=400
+        )
+        
+        clients = self.service.list_client()
+        
+        
+        table = ft.DataTable(
+            columns=[
+                ft.DataColumn(ft.Text("Nome")),
+                ft.DataColumn(ft.Text("Email")),
+                ft.DataColumn(ft.Text("Deletar"))
+            ],
+            rows=[]
+        )
+        
+        
+        def fill_table(query="") -> None:
+            query = query.lower()
+            filtered = [
+                c for c in clients
+                if query in c.name.lower()
+                or query in c.email.lower()
+            ] if query else clients
+            
+            table.rows.clear()
+            
+            for c in filtered:
+                table.rows.append(
+                    ft.DataRow(
+                        cells=[
+                            ft.DataCell(ft.Text(c.name)),
+                            ft.DataCell(ft.Text(c.email)),
+                            ft.DataCell(ft.IconButton("delete", icon_color="red", data=c.id, on_click=self.delete_client))
+                        ]
+                    )
+                )
+            
+            if table.page:
+                table.update()
+        
+        search_input.on_change = lambda e: (fill_table(e.control.value), table.update())
+        
+        fill_table()
+        
+        form_datable = ft.Container(
+            content=ft.Column(
+                controls=[
+                    search_input,
+                    table
+                ],
+                alignment=ft.MainAxisAlignment.CENTER,
+                scroll=ft.ScrollMode.ALWAYS,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            ),
+            alignment=ft.alignment.center,
+            padding=20
+        )
+            
+        return form_datable
+        
+    def delete_client(self,e):
+        client_id = e.control.data
+        self.service.delete_client(client_id)
+        self.refresh_client()
+    
